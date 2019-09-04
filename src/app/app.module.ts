17e98933808e30b3773bbf6972679e8fa009d060
Dirck Hecking <dhecking@gmail.com>
2019-09-04 06:04:57 -0400

    angular training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
