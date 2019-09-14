1927978aeb4964052c3136632939a32277c68789
Dirck Hecking <dhecking@gmail.com>
2019-09-14 12:34:38 -0400

    smhfml

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# smhfml
+# Adventure Maps
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.2.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('smhfml app is running!');
+    expect(page.getTitleText()).toEqual('Adventure Maps is running!');
   });
 
   afterEach(async () => {
