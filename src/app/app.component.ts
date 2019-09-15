f781d5aed656a4f12ee28ead509810fa5980dd66
Dirck Hecking <dhecking@gmail.com>
2019-09-14 22:42:55 -0400

    adding autoupdate code for pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import * as firebase from 'firebase/app';
 // import 'firebase/<PACKAGE>'
 
@@ -15,8 +16,33 @@ const config = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit, OnDestroy {
+  swUpdate: any;
+  newVersion: true;
+
+  constructor(swUpdate: SwUpdate) {
+    console.log("AppComponent::constructor()", swUpdate);
+    this.swUpdate = swUpdate;
+  }
+
+  ngOnInit(): void {
+    console.log("AppComponent::ngOnInit");
+    this.swUpdate.available.subscribe(update => this.newVersion = true);
     firebase.initializeApp(config);
   }
+
+  ngOnDestroy(): void {
+    console.log("AppComponent::ngOnDestroy");
+  }
+
+  reload() {
+    console.log("AppComponent::reload");
+    this.swUpdate.activated.subscribe(
+      update => {
+        // console.log(update);
+        window.location.reload();
+      },
+      this.swUpdate.activateUpdate());
+  }
+
 }
