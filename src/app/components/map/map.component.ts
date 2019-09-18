58cc50527c7ccbdcda6506c1847c3e00765cf845
Dirck Hecking <dhecking@gmail.com>
2019-09-18 14:01:26 -0400

    hmmm

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,31 @@
 import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
+import * as bluemoon from './styles/bluemoon.json';
+
+const MapOpts: google.maps.MapOptions = {
+  disableDefaultUI: true,
+  gestureHandling: "none",
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  zoom: 18,
+  backgroundColor: "#1d2c4d",
+  noClear: true,
+  // styles: bluemoon
+};
+
+const SymOpts: google.maps.Symbol = {
+  path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+  fillColor: "#bff",
+  fillOpacity: 0.8,
+  scale: 10,
+  strokeColor: '#4cf',
+  strokeWeight: 2
+};
+
+const GeoOpts: PositionOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
 @Component({
   selector: "app-map",
@@ -9,31 +35,19 @@ import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef } fr
 export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   map: google.maps.Map;
   marker: google.maps.Marker;
-
+  geoSpeed: number = 0;
+  geoHeading: number = 0;
+  geoAltitude: number = 0;
   geoLocation: google.maps.LatLng;
-  geoSpeed: number;
-  geoHeading: number;
-  geoAltitude: number;
-
   deviceAbsolute: boolean = true;
   deviceBank: number = 0;
   devicePitch: number = 0;
   deviceDirection: number = 0;
   deviceHeading: number = 0;
   deviceHeadingAccuracy: number = 0;
-
   watchPositionId: number;
-  options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
 
   @ViewChild("googlemaps", { static: false }) mapElement: ElementRef;
-
-  constructor() {
-
-  }
 
   ngOnInit(): void {
     console.log("MapComponent::ngOnInit");
@@ -42,7 +56,6 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     console.log("MapComponent::ngAfterViewInit");
-    // this.hideAttributions();
   }
 
   ngOnDestroy(): void {
@@ -51,83 +64,50 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   initMap() {
-    console.log("MapComponent::initMap");
 
     if (navigator.geolocation) {
+
       navigator.geolocation.getCurrentPosition(
-        position => {
+        (position: Position) => {
           this.geoLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-          this.map = new google.maps.Map(this.mapElement.nativeElement, {
-            zoom: 18,
-            backgroundColor: "#1d2c4d",
-            noClear: true,
-            center: this.geoLocation,
-            disableDefaultUI: true,
-            rotateControl: true,
-            gestureHandling: "none",
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: this.getCustomStyle()
-          });
 
-          this.marker = new google.maps.Marker({
-            position: this.geoLocation,
-            title: "Dirck Hecking",
-            icon: this.getSymbol(),
-            map: this.map
-          });
+          this.map = new google.maps.Map(this.mapElement.nativeElement, MapOpts);
+          this.map.setCenter(this.geoLocation);
 
-          this.marker.addListener('click', () => {
-            console.log("MapComponent.marker::click()");
-          });
+          this.marker = new google.maps.Marker();
+          this.marker.setMap(this.map);
+          this.marker.setPosition(this.geoLocation);
+          this.marker.setIcon(SymOpts);
 
         },
-        error => {
-          console.error("MapComponent: " + error.message);
+        (error: PositionError) => {
+          console.error(error.message);
         },
-        this.options
+        GeoOpts
       );
 
       this.watchPositionId = navigator.geolocation.watchPosition(
-        position => {
+        (position: Position) => {
           this.geoLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+
           this.map.panTo(this.geoLocation);
           this.marker.setPosition(this.geoLocation);
           this.updateDashboard(position);
+
         },
-        error => {
-          console.error("MapComponent: " + error.message);
+        (error: PositionError) => {
+          console.error(error.message);
         },
-        this.options
+        GeoOpts
       );
 
-      window.addEventListener('deviceorientation', (event: DeviceOrientationEvent) => {
-        this.marker.setIcon(this.getSymbol());
-        this.displayDeviceOrientation(event);
-      }, true);
+      window.addEventListener('deviceorientation',
+        (event: DeviceOrientationEvent) => {
+          this.displayDeviceOrientation(event);
+        },
+        true);
 
     }
-  }
-
-  bounceMarker(marker: google.maps.Marker) {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  getSymbol(): google.maps.Symbol {
-    const symbol = {
-      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-      fillColor: "#bff",
-      fillOpacity: 0.8,
-      scale: 10,
-      rotation: this.deviceHeading,
-      strokeColor: '#4cf',
-      strokeWeight: 2
-    };
-    return symbol;
-
   }
 
   updateDashboard(position: Position) {
@@ -140,7 +120,7 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   displayDeviceOrientation(event: DeviceOrientationEvent) {
-    this.deviceAbsolute = event.absolute || false;
+    this.deviceAbsolute = event.absolute;
     this.deviceBank = parseInt(Math.round(event.alpha).toFixed(0), 10);
     this.devicePitch = parseInt(Math.round(event.beta).toFixed(0), 10);
     this.deviceDirection = parseInt(Math.round(event.gamma).toFixed(0), 10);
@@ -148,277 +128,4 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
     document.getElementById("deviceHeading").innerText = this.deviceHeading + "";
   }
 
-  hideAttributions() {
-    console.log("MapComponent::hideAttributions");
-    const sleep = (milliseconds: number) => {
-      return new Promise(resolve => setTimeout(resolve, milliseconds));
-    };
-    sleep(1500).then(() => {
-      const items = document.querySelectorAll(".gmnoprint");
-      // console.log("hideAttributionsRight: " + items.length);
-      items.forEach(item => {
-        const element = item as HTMLElement;
-        element.style.display = "none";
-      });
-      const items2 = document.querySelectorAll("[rel=noopener]");
-      // console.log("hideAttributionsLeft: " + items2.length);
-      items2.forEach(item => {
-        const element = item as HTMLElement;
-        element.style.display = "none";
-      });
-    });
-  }
-
-
-  getCustomStyle(): any {
-    return [
-      {
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#1d2c4d"
-          }
-        ]
-      },
-      {
-        elementType: "labels",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#8ec3b9"
-          }
-        ]
-      },
-      {
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#1a3646"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#4b6878"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#64779e"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.neighborhood",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      },
-      {
-        featureType: "administrative.province",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#4b6878"
-          }
-        ]
-      },
-      {
-        featureType: "landscape.man_made",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#334e87"
-          }
-        ]
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#023e58"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#283d6a"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#6f9ba5"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#1d2c4d"
-          }
-        ]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#023e58"
-          }
-        ]
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#3C7680"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#304a7d"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#98a5be"
-          }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#1d2c4d"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#2c6675"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            color: "#255763"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#b0d5ce"
-          }
-        ]
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#023e58"
-          }
-        ]
-      },
-      {
-        featureType: "transit",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#98a5be"
-          }
-        ]
-      },
-      {
-        featureType: "transit",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#1d2c4d"
-          }
-        ]
-      },
-      {
-        featureType: "transit.line",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#283d6a"
-          }
-        ]
-      },
-      {
-        featureType: "transit.station",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#3a4762"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#0e1626"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#4e6d70"
-          }
-        ]
-      }
-    ];
-  }
 }
