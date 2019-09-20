927d608b3f9ccd3be05a1a6269270b8bb778d4e7
Dirck Hecking <dhecking@gmail.com>
2019-09-20 19:01:27 -0400

    hmm

--- server/server.js
+++ server/server.js
@@ -42,5 +42,5 @@ app.post('/notifications', (req, res) => {
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`);
 });
