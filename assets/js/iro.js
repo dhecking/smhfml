9c9a73de457f27af96ce2bbcf7306f029ed9d2c5
Dirck Hecking <dhecking@gmail.com>
2019-09-26 09:22:05 -0400

    Auto-generated commit

---
+++ assets/js/iro.js
@@ -0,0 +1,15 @@
+var colorWheel = iro.ColorWheel("#colorWheelDemo", {
+  width: 320,
+  height: 320,
+  padding: 4,
+  markerRadius: 8,
+  color: "rgb(68, 255, 158)",
+  css: {
+    "#logo": {
+      "stroke": "rgb"
+    },
+    ".subtitle": {
+      "color": "rgb"
+    }
+  }
+});
